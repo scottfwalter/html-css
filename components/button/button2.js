fcfeef450918d26d23bf93329e0cf2551e0b5bb9
Scott Walter <scott.walter@nice.com>
2021-09-08 21:42:04 -0500

    migrated

---
+++ components/button/button2.js
@@ -0,0 +1,7 @@
+var button = document.querySelector("span");
+      button.addEventListener("click" ,()=>{
+          button.innerHTML = `<div class="loading"></div>`
+          setTimeout(() => {
+              button.innerText = "Submited"
+          }, 5000);
+      })
