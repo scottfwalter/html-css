252ba588c8876faa2cdb68ff9f36ad5abedba851
Scott Walter <scott.walter@nice.com>
2022-04-15 15:31:40 -0500

    animations

---
+++ dialog/dialog-animated.js
@@ -0,0 +1,20 @@
+const modal = document.querySelector("#modal");
+const openModal = document.querySelector(".open-button");
+const closeModal = document.querySelector(".close-button");
+
+openModal.addEventListener("click", () => {
+  modal.showModal();
+});
+
+closeModal.addEventListener("click", () => {
+  modal.setAttribute("closing", "");
+
+  modal.addEventListener(
+    "animationend",
+    () => {
+      modal.removeAttribute("closing");
+      modal.close();
+    },
+    { once: true }
+  );
+});
